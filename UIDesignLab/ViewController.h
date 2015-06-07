38f4bd396657b32d0e82555ae3325ef24ebfe29f
Sebastian Qu <sebastianqu@gmail.com>
2015-06-07 21:57:02 +0800

    Initial Commit

---
+++ UIDesignLab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIDesignLab
+//
+//  Created by 瞿盛 on 15/6/7.
+//  Copyright (c) 2015年 Sebastian Qu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIDesignLab/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIDesignLab
+//
+//  Created by 瞿盛 on 15/6/7.
+//  Copyright (c) 2015年 Sebastian Qu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
